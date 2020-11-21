a6f2c36efa3e47e5d7736ddf06bfb22da114674e
Pablo Cha <pabloc@fulljaus.com>
2020-11-21 20:34:01 -0300

    v1.0.0 - Se ordenan los resultado aplicando Sørensen–Dice

--- app/controllers/searchable_controller.rb
+++ app/controllers/searchable_controller.rb
@@ -3,18 +3,60 @@ class SearchableController < ApplicationController
     entity_type = params[:controller]
     pattern = params[:q]
     pattern_bgrams = Wh2find.get_bgrams_from pattern
-    matching_indexes = Wh2find::Index
-                           .where(entity: entity_type)
-                           .limit(10)
-    indexable_entity_hash = Wh2find::INDEXABLES.bsearch { |x| x[:entity_name] == entity_type}
+    match_entity = {'$match': {entity: entity_type}}
+    project = {
+        '$project' => {
+            _id: 1,
+            matching_ids: 1,
+            bgrams: 1,
+            score: {
+                "$divide": [
+                    {
+                        "$multiply": [2, {
+                            "$size": {
+                                "$setIntersection": ["$bgrams", pattern_bgrams]
+                            }
+                        }
+                        ]
+                    },
+                    {
+                        "$sum": [
+                            {"$size": "$bgrams"},
+                            pattern_bgrams.size
+
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+    sort_by_score = {
+        '$sort' => {
+            score: -1
+        }
+    }
+    unwind_by_matching_ids = {
+        '$unwind' => "$matching_ids"
+    }
+    limit_results = {
+        "$limit" => 10
+    }
+    matching_indexes = Wh2find::Index.collection.aggregate([
+                                                               match_entity,
+                                                               project,
+                                                               sort_by_score,
+                                                               unwind_by_matching_ids,
+                                                               limit_results
+
+                                                           ])
+    indexable_entity_hash = Wh2find::INDEXABLES.bsearch { |x| x[:entity_name] == entity_type }
     searchable_entity = indexable_entity_hash[:entity]
-    found_entities = []
-    matching_indexes.each do |index|
-      found_entities += index.matching_ids.map do |entity_id|
-        entity_id = entity_id.to_s
-        searchable_entity.find entity_id
-      end
-    end
+    found_entities = matching_indexes.as_json.map {|result| searchable_entity.find get_entity_id_from result
+    }
     @response = found_entities.as_json
+  end
+
+  def get_entity_id_from result
+    result["matching_ids"].first.last
   end
 end

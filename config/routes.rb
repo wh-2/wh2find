e6130bf5888d09fe2ed69c6e3becc455a4e2de16
Pablo Cha <pabloc@fulljaus.com>
2021-01-30 12:53:08 -0300

    v2.0.2 - implementable sin configurar
    Changelog:
     - El router valida si está el engine configurado (existe la constatne Wh2find::INDEXABLES) antes de intentar agregar la ruta. Esto permite implementar el engoin sólo por sus concerns, pero sin interesarnos la funcionalidad de indexar o buscar desde el proyecto que la incluye

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   Wh2find::INDEXABLES.each do |indexable_entity|
     entity_name = indexable_entity[:entity_name]
     get "#{entity_name}/search", controller: entity_name, action: "search"
-  end
+  end if Wh2find.const_defined? 'INDEXABLES'
 end
--- lib/wh2find/version.rb
+++ lib/wh2find/version.rb
@@ -1,3 +1,3 @@
 module Wh2find
-  VERSION = '2.0.1'
+  VERSION = '2.0.2'
 end
